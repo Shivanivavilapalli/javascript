7e596da8716c51d700b03ac247043e3880473379
jfsbatch10 <jfsbatch10@gmail.com>
2026-06-16 10:10:35 +0530

    added basic javascript code

--- script.js
+++ script.js
@@ -27,16 +27,41 @@
 //     console.log("not leap year");
 // }
 
-let n = 3,z=0;
-for(let i=2;i<n;i++){
-    if(n%i==0){
-        break;
-        z=1;
-    }
+// let n = 3,z=0;
+// for(let i=2;i<n;i++){
+//     if(n%i==0){
+//         break;
+//         z=1;
+//     }
         
-}
-if(z==0)
-    console.log("prime number");
-else        
-    console.log("not prime number");
+// }
+// if(z==0)
+//     console.log("prime number");
+// else        
+//     console.log("not prime number");
+// let head = document.getElementsByTagName("h1");
+// head[1].innerText = "byee buee 1"
+// console.log(head[0].innerText);
+// let para = document.getElementById("para");
+// para.textContent
+// para.innerText = "<b>adios</b>"
+// para.innerHTML = "<b>adios</b>"
+// let h = document.getElementsByClassName('A');
+// h[1].innerHTML = "changed ver"
+// const change =() =>{
+//    let di =  document.getElementById("display");
+//     di.innerHTML = "bye";
+//     di.style.backgroundColor ='pink';
+//     di.style.color = 'white';
+// }
+const change = () =>{
+    // let di = document.getElementById("display");
+    // di.style.backgroundColor = "red"
+    const colors = ["red", "blue", "green", "yellow", "orange", "purple"];
 
+    let index = Math.floor(Math.random() * colors.length);
+
+    document.body.style.backgroundColor = colors[index];
+}
+// use 
+// Math.random() Math.floor() arrY
